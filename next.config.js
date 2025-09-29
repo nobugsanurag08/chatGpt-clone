b0db1cd9cf75caff0dd94cd2f0dc69e1b6c0ae48
anuragkumar-22 <anuragk.ug19.cs@nitp.ac.in>
2025-09-29 13:25:01 +0530

    added next comptibility with vercel

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['@clerk/nextjs'],
+  },
 };
 
 module.exports = nextConfig;
